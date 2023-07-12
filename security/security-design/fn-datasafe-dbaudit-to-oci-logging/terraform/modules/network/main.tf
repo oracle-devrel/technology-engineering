fd0828fb64d180c1ff195ae2437092b75c2a6151
fzarrioracle <fabrizio.zarri@oracle.com>
2023-07-12 20:30:34 +0200

    Update some typos

--- security/security-design/fn-datasafe-dbaudit-to-oci-logging/terraform/modules/network/main.tf
+++ security/security-design/fn-datasafe-dbaudit-to-oci-logging/terraform/modules/network/main.tf
@@ -1,46 +1,15 @@
 ################################################################################
-#Copyright (c) 2023 Oracle and/or its affiliates.
-# 
-#The Universal Permissive License (UPL), Version 1.0
-# 
-#Subject to the condition set forth below, permission is hereby granted to any
-#person obtaining a copy of this software, associated documentation and/or data
-#(collectively the "Software"), free of charge and under any and all copyright
-#rights in the Software, and any and all patent rights owned or freely
-#licensable by each licensor hereunder covering either (i) the unmodified
-#Software as contributed to or provided by such licensor, or (ii) the Larger
-#Works (as defined below), to deal in both
-# 
-#(a) the Software, and
-#(b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
-#one is included with the Software (each a "Larger Work" to which the Software
-#is contributed by such licensors),
- 
-#without restriction, including without limitation the rights to copy, create
-#derivative works of, display, perform, and distribute the Software and make,
-#use, sell, offer for sale, import, export, have made, and have sold the
-#Software and the Larger Work(s), and to sublicense the foregoing rights on
-#either these or other terms.
- 
-#This license is subject to the following condition:
-#The above copyright notice and either this complete permission notice or at
-#a minimum a reference to the UPL must be included in all copies or
-#substantial portions of the Software.
- 
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#SOFTWARE.
+# Copyright (c) 2023, Oracle and/or its affiliates. All rights reserved.
+# This software is dual-licensed to you under the Universal Permissive 
+# License (UPL) 1.0 as shown at https://oss.oracle.com/licenses/upl
 ################################################################################
+
 
 resource "oci_core_virtual_network" "vcn" {
   cidr_block     = var.VCN-CIDR
-  dns_label      = "${var.fnvcndnslabelprefix}${random_id.tag.hex}"
+  dns_label      = "${var.vcndnslabelprefix}${random_id.tag.hex}"
   compartment_id = var.compartment_ocid
-  display_name   = "${var.fnvcnnameprefix}-${random_id.tag.hex}"
+  display_name   = "${var.vcnnameprefix}-${random_id.tag.hex}"
 }
 
 data "oci_core_services" "service_gateway_all_oci_services" {
@@ -58,19 +27,19 @@ resource "oci_core_service_gateway" "service_gw" {
   services {
         service_id = lookup(data.oci_core_services.service_gateway_all_oci_services.services[0], "id")
   }
-  display_name = "${var.fnvcnnameservicegatewayprefix}-${random_id.tag.hex}"
+  display_name = "${var.vcnnameservicegatewayprefix}-${random_id.tag.hex}"
 }
 
 resource "oci_core_route_table" "rt_fn_subnet" {
   compartment_id = var.compartment_ocid
   vcn_id         = oci_core_virtual_network.vcn.id
-  display_name   = "${var.fnvcnnameroutingtableprefix}-${random_id.tag.hex}"
+  display_name   = "${var.vcnnameroutingtableprefix}-${random_id.tag.hex}"
   
   route_rules {
     destination       = lookup(data.oci_core_services.service_gateway_all_oci_services.services[0], "cidr_block")
     destination_type  = "SERVICE_CIDR_BLOCK"
     network_entity_id = oci_core_service_gateway.service_gw.id
-    description = "${var.fnvcnroutingtabledescriptionervicegw}-${random_id.tag.hex}"
+    description = "${var.vcnroutingtabledescriptionservicegw}-${random_id.tag.hex}"
   }
   
 }
@@ -80,7 +49,7 @@ resource "oci_core_route_table" "rt_fn_subnet" {
 resource "oci_core_dhcp_options" "dhcpoptions1" {
   compartment_id = var.compartment_ocid
   vcn_id         = oci_core_virtual_network.vcn.id
-  display_name   = "${var.fnvcnnamedhcpopitonsprefix}-${random_id.tag.hex}"
+  display_name   = "${var.vcnnamedhcpopitonsprefix}-${random_id.tag.hex}"
   options {
     type        = "DomainNameServer"
     server_type = "VcnLocalPlusInternet"
@@ -88,9 +57,9 @@ resource "oci_core_dhcp_options" "dhcpoptions1" {
 }
 
 resource "oci_core_subnet" "function_ds_log_vcn" {
-  cidr_block        = var.fnsubnet-CIDR
-  display_name      = "${var.fnsubnetnameprefix}-${random_id.tag.hex}"
-  dns_label         = "${var.fnsubnetdnslabelprefix}${random_id.tag.hex}"
+  cidr_block        = var.subnet-CIDR
+  display_name      = "${var.subnetnameprefix}-${random_id.tag.hex}"
+  dns_label         = "${var.subnetdnslabelprefix}${random_id.tag.hex}"
   compartment_id    = var.compartment_ocid
   vcn_id            = oci_core_virtual_network.vcn.id
   route_table_id    = oci_core_route_table.rt_fn_subnet.id
@@ -103,7 +72,7 @@ resource "oci_core_subnet" "function_ds_log_vcn" {
 resource "oci_core_security_list" "vcn_security_list"{
   compartment_id = var.compartment_ocid
   vcn_id         = oci_core_virtual_network.vcn.id
-  display_name = "${var.fnvcnnamesecuritylistprefix}-${random_id.tag.hex}"
+  display_name = "${var.vcnnamesecuritylistprefix}-${random_id.tag.hex}"
 
   egress_security_rules {
       stateless = false
--- security/security-design/fn-datasafe-dbaudit-to-oci-logging/terraform/modules/network/variables.tf
+++ security/security-design/fn-datasafe-dbaudit-to-oci-logging/terraform/modules/network/variables.tf
@@ -1,40 +1,9 @@
 ################################################################################
-#Copyright (c) 2023 Oracle and/or its affiliates.
-# 
-#The Universal Permissive License (UPL), Version 1.0
-# 
-#Subject to the condition set forth below, permission is hereby granted to any
-#person obtaining a copy of this software, associated documentation and/or data
-#(collectively the "Software"), free of charge and under any and all copyright
-#rights in the Software, and any and all patent rights owned or freely
-#licensable by each licensor hereunder covering either (i) the unmodified
-#Software as contributed to or provided by such licensor, or (ii) the Larger
-#Works (as defined below), to deal in both
-# 
-#(a) the Software, and
-#(b) any piece of software and/or hardware listed in the lrgrwrks.txt file if
-#one is included with the Software (each a "Larger Work" to which the Software
-#is contributed by such licensors),
- 
-#without restriction, including without limitation the rights to copy, create
-#derivative works of, display, perform, and distribute the Software and make,
-#use, sell, offer for sale, import, export, have made, and have sold the
-#Software and the Larger Work(s), and to sublicense the foregoing rights on
-#either these or other terms.
- 
-#This license is subject to the following condition:
-#The above copyright notice and either this complete permission notice or at
-#a minimum a reference to the UPL must be included in all copies or
-#substantial portions of the Software.
- 
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#SOFTWARE.
+# Copyright (c) 2023, Oracle and/or its affiliates. All rights reserved.
+# This software is dual-licensed to you under the Universal Permissive 
+# License (UPL) 1.0 as shown at https://oss.oracle.com/licenses/upl
 ################################################################################
+
 
 
 variable "compartment_ocid" {
@@ -46,42 +15,42 @@ variable "VCN-CIDR" {
   description = "The CIDR block of VCN"
 }
 
-variable "fnsubnet-CIDR" {
+variable "subnet-CIDR" {
   default = "10.0.1.0/24"
 }
 
-variable "fnvcndnslabelprefix" {
-  default = "fndslogvcn"
+variable "vcndnslabelprefix" {
+  default = "dnsepl"
 }
 
-variable "fnvcnnameprefix" {
-  default = "vcn_fn_ds_log-fndslog"
+variable "vcnnameprefix" {
+  default = "vnc"
   description = "The prefix display name of VCN"
 }
 
-variable "fnvcnnameroutingtableprefix" {
-  default = "function_ds_log_vcn_routetable"
+variable "vcnnameroutingtableprefix" {
+  default = "rt"
 }
 
-variable "fnvcnroutingtabledescriptionervicegw" {
+variable "vcnroutingtabledescriptionservicegw" {
   default = "Route for Service Gateway"
 }
 
-variable "fnvcnnamedhcpopitonsprefix" {
-  default = "function_ds_log_vcn_dhcpoptions1"
+variable "vcnnamedhcpopitonsprefix" {
+  default = "dhcpo"
 }
 
-variable "fnsubnetnameprefix" {
-  default = "private_fn_ds_log_vcn_subnet"
+variable "subnetnameprefix" {
+  default = "sub"
 }
 
-variable "fnsubnetdnslabelprefix" {
-  default = "fndslogsub"
+variable "subnetdnslabelprefix" {
+  default = "dnsepl"
 }
 
-variable "fnvcnnamesecuritylistprefix" {
-  default = "functions-security-list"
+variable "vcnnamesecuritylistprefix" {
+  default = "sl"
 }
-variable "fnvcnnameservicegatewayprefix" {
-  default = "function_ds_log_vcn_sg"
+variable "vcnnameservicegatewayprefix" {
+  default = "sgw"
 }
