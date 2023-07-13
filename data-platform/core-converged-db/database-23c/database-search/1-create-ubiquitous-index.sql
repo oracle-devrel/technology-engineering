fc994ed27168d24eb7eef6d8e15aaa0c0e614bc2
Ulrike Schwinn <ulrike.schwinn@oracle.com>
2023-07-13 14:20:03 +0200

    Create 1-create-ubiquitous-index.sql

---
+++ data-platform/core-converged-db/database-23c/database-search/1-create-ubiquitous-index.sql
@@ -0,0 +1,14 @@
+REM Script for 23c: 1-create-ubiquitous-index.sql
+REM Create an ubiquitous index   
+ 
+-- prereq: install Oracle Customer order sample schema from https://github.com/oracle-samples/db-sample-schemas/tree/main/customer_orders
+-- create an ubiquitous index with the name SEARCH_PRODUCTS as user CO
+
+execute DBMS_SEARCH.CREATE_INDEX(index_name=>'SEARCH_PRODUCTS');
+
+-- it creates the infrastructure for Oracle Text including a table SEARCH_PRODUCTS with two JSON columns DATA and METADATA.
+
+desc SEARCH_PRODUCTS
+
+-- if you need to drop the index first use 
+-- execute DBMS_SEARCH.DROP_INDEX('SEARCH_PRODUCTS');
