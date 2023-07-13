67810ac79097fcb0c4b25bfff6235483b69f4071
Ulrike Schwinn <ulrike.schwinn@oracle.com>
2023-07-13 14:59:10 +0200

    Create 7-monitoring-index.sql

---
+++ data-platform/core-converged-db/db-search/7-monitoring-index.sql
@@ -0,0 +1,11 @@
+REM Script for 23c: 7-monitoring-index.sql
+
+-- Query USER_INDEXES to get basic information about the created index.
+
+select index_type, table_owner, table_name, table_type, status, ityp_owner
+from user_indexes where index_name='SEARCH_PRODUCTS';
+
+-- Query CTX_INDEX_VALUES to display values for each object used in the index. 
+
+select ixv_class, ixv_object, ixv_attribute, ixv_value
+from ctxsys.CTX_INDEX_VALUES where ixv_index_owner='CO';
