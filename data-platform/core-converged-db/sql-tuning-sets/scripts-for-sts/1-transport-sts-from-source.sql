b763bb4dba6b3550a150a72076ff21c92c7abf14
Ulrike Schwinn <ulrike.schwinn@oracle.com>
2023-07-21 11:49:54 +0200

    Create 1-transport-sts-from-source.sql

---
+++ data-platform/core-converged-db/sql-tuning-sets/scripts-for-sts/1-transport-sts-from-source.sql
@@ -0,0 +1,16 @@
+REM Script: 1-transport-sts-from-source.sql
+REM Create a staging table and add the statements to it (source system) 
+
+-- Use CREATE_STGTAB_SQLSET to create the staging table
+
+execute dbms_sqltune.create_stgtab_sqlset(table_name => '&tablename', schema_name => '&owner');
+
+-- Load the staging table with data from one or more STS with PACK_STGTAB_SQLSET
+
+execute dbms_sqltune.pack_stgtab_sqlset(sqlset_name => '&stsname', -
+                                        sqlset_owner => '&owner', -
+                                        staging_table_name => '&stagingtab_name',- 
+                                        staging_schema_owner=> '&stagingtab_owner');
+
+
+-- use datapump to export the staging table and import it to the target system
