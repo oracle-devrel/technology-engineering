3876d13d2d23aaa468512e61bd9e8aef9058e50b
fzarrioracle <fabrizio.zarri@oracle.com>
2023-07-21 17:38:22 +0200

    update code in according with comments

--- security/security-design/fn-datasafe-dbaudit-to-oci-logging/terraform/function/oci-datasafe-audit-to-logging/func.py
+++ security/security-design/fn-datasafe-dbaudit-to-oci-logging/terraform/function/oci-datasafe-audit-to-logging/func.py
@@ -18,7 +18,7 @@ from oci.util import to_dict
 from fdk import response
 from datetime import datetime, timedelta
 
-#Set Logging level
+#Set Logging level.
 #logger.setLevel(logging.DEBUG)
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -90,7 +90,7 @@ def get_audit_events(data_safe_cl,l_compartment_id, l_sort_order, l_limit, l_sor
                                                                     scim_query=l_scim_query
                 )
         
-        #Add audit events in pandas Dataframe
+        #Add audit events in pandas Dataframe.
         ds_audit = pd.DataFrame()
         ds_audit=pd.json_normalize(to_dict(audit_events_response.data), record_path='items')
         #Paging audit events
@@ -105,18 +105,18 @@ def get_audit_events(data_safe_cl,l_compartment_id, l_sort_order, l_limit, l_sor
                                                                     scim_query=l_scim_query,
                                                                     page=audit_events_response.next_page
             )
-            #Add audit events in pandas Dataframe
+            #Add audit events in pandas Dataframe.
             ds_audit=pd.concat([ds_audit,pd.json_normalize(to_dict(audit_events_response.data), record_path='items')],verify_integrity=True, ignore_index=True)
             logger.info("Paging List audit events from Data Safe")
             logger.info("Number of audit events imported:  %s", len(ds_audit))
         if (not ds_audit.empty):
-            #To Camel Dataframe Headers
+            #To Camel Dataframe Headers.
             ds_audit.columns = map(to_camel_case, ds_audit.columns)
-            #Convert timeCollected column in datatime format
+            #Convert timeCollected column in datatime format.
             ds_audit[headerTimeCollected] = pd.to_datetime(ds_audit[headerTimeCollected], format='mixed', yearfirst=True, utc=True)
-            #Sort DataFrame by 'timeCollected' value
+            #Sort DataFrame by 'timeCollected' value.
             ds_audit = ds_audit.sort_values(by=headerTimeCollected, ascending=False, ignore_index=True)
-            #Rebuild index of DataFrame
+            #Rebuild index of DataFrame.
             ds_audit = ds_audit.reset_index(drop=True)
     except Exception as e:
         logger.error("List Audits from Data Safe Error: %s", e)
@@ -150,7 +150,7 @@ def to_camel_case(text):
     return "".join([w.capitalize() if w != words[0] else w for w in words])
 
 def check_object_from_bucket(l_bucketName, l_objectName, ol_client):
-    #Check if object file is in OCI ObjectStorage/Bucket
+    #Check if object file is in OCI ObjectStorage/Bucket.
     logger.debug("Check object file " + l_objectName + "is in a Bucket")
     l_namespace = ol_client.get_namespace().data
     try:
@@ -160,7 +160,7 @@ def check_object_from_bucket(l_bucketName, l_objectName, ol_client):
                                                        )
         if list_objects_response.data.objects:
             for o in list_objects_response.data.objects:
-                #File already present in bucket
+                #File already present in bucket.
                 logger.debug("ObjectName: " + str(o.name))
                 if o.name == l_objectName:
                     logger.debug("Object file " + l_objectName + " is in a Bucket")
@@ -169,7 +169,7 @@ def check_object_from_bucket(l_bucketName, l_objectName, ol_client):
                     logger.debug("Object file " + l_objectName + " is not present in a Bucket")
                     fileExist = False
         else:
-            #File is not present in bucket
+            #File is not present in bucket.
             logger.debug("Object file " + l_objectName + " is not present in a Bucket")
             fileExist = False    
     except Exception as e:
@@ -180,15 +180,15 @@ def check_object_from_bucket(l_bucketName, l_objectName, ol_client):
     return fileExist
 
 def check_file_lock_bucket(lo_bucketName, lo_objectName, lo_client, lo_fntimeout, lo_current_time):
-    #Check if lock file is in OCI ObjectStorage/Bucket
+    #Check if lock file is in OCI ObjectStorage/Bucket.
     logger.debug("Check lock file is in a Bucket")
     lockFilePresent = False
     lockFileExist = check_object_from_bucket(lo_bucketName, lo_objectName, lo_client)
     if lockFileExist:
         logger.debug("Check if lock file last modified time is > of fn execution")
-        #Get Last Modified Time lock file from bucket
+        #Get Last Modified Time lock file from bucket.
         oblastmodifiedtime_str = get_object_last_modified_time_from_bucket(lo_bucketName, lo_objectName, lo_client)
-        #Covert data in datatimeFormat date: Thu, 15 Jun 2023 10:25:17 GMT
+        #Covert data in datatimeFormat date: Thu, 15 Jun 2023 10:25:17 GMT.
         oblastmodifiedtime = datetime.strptime(oblastmodifiedtime_str, '%a, %d %b %Y %H:%M:%S %Z')
         oblastmodifiedtime = oblastmodifiedtime + lo_fntimeout
 
@@ -205,7 +205,7 @@ def check_file_lock_bucket(lo_bucketName, lo_objectName, lo_client, lo_fntimeout
     return lockFilePresent
 
 def get_object_from_bucket(r_bucketName, r_objectName, or_client, r_lastAuditEventRecordTime_attr):
-    #Get cursor file with last execution in OCI ObjectStorage/Bucket
+    #Get cursor file with last execution in OCI ObjectStorage/Bucket.
     logger.debug("Get object " + r_objectName + " file from Bucket")
     r_namespace = or_client.get_namespace().data
     try:
@@ -221,7 +221,7 @@ def get_object_from_bucket(r_bucketName, r_objectName, or_client, r_lastAuditEve
     return r_lastexecutionupdatime
 
 def delete_object_from_bucket(d_bucketName, d_objectName, d_client):
-    #Delete object from bucket in OCI ObjectStorage/Bucket
+    #Delete object from bucket in OCI ObjectStorage/Bucket.
     logger.debug("Delete object " + d_objectName + " Bucket")
     d_namespace = d_client.get_namespace().data
     try:
@@ -234,7 +234,7 @@ def delete_object_from_bucket(d_bucketName, d_objectName, d_client):
     
 
 def get_object_last_modified_time_from_bucket(h_bucketName, h_objectName, h_client):
-    #Get last_modified_time header from file in bucket
+    #Get last_modified_time header from file in bucket.
     logger.debug("Get last_modified_time header from object " + h_objectName + " in bucket")
     h_namespace = h_client.get_namespace().data
     try:
@@ -279,7 +279,7 @@ def generate_scim_query(q_last_time_collected, q_actual_time):
 def main(ctx):
     
 
-    #Initializing Variable
+    #Initializing Variables.
     limit = 10000
     access_level = "ACCESSIBLE"
     sort_by = "timeCollected"
@@ -312,7 +312,7 @@ def main(ctx):
             "OCI OS BucketName: " + ociOSTrackerBucketName
         )
         
-        #Calculate fn timeout configured for manage validity of lock file
+        #Calculate fn timeout configured for manage validity of lock file.
         fndeadlinetime_str = ctx.Deadline()
         fndeadlinetime = datetime.strptime(fndeadlinetime_str, '%Y-%m-%dT%H:%M:%SZ')
         current_time_t = datetime.utcnow()
@@ -329,62 +329,62 @@ def main(ctx):
         logger.debug("initializing RPS client")
         rps = init_rps_client()
         logger.debug("RPS client obtain with Instance Principal")
-        # Step 2: Initializing ObjectStorage/Bucket Client
+        # Step 2: Initializing ObjectStorage/Bucket Client.
         logger.debug("initializing ObjectStorage/Bucket client")
         os_client = init_objectstorage_client(rps)
         logger.debug("ObjectStorage/Bucket client obtained with Instance Principal")
-        # Step 3: Check file lock in ObjectStorage/Bucket 
+        # Step 3: Check file lock in ObjectStorage/Bucket.
         logger.debug("Manage file lock in Bucket")
         check_file_lock = check_file_lock_bucket(ociOSTrackerBucketName, lock_file_name, os_client, fntimeout, current_time_t)
         if check_file_lock:
-            #File lock is present and valid other fn execution is active
+            #File lock is present and valid other fn execution is active.
             logger.info("File Lock is valid other fn session is yet in execution")
         else:
-            # Step 4: Check if exist file cursor in ObjectStorage/Bucket 
+            # Step 4: Check if exist file cursor in ObjectStorage/Bucket. 
             logger.debug("Check if File cursor exist in Bucket")
             check_existence_cursor_file = check_object_from_bucket(ociOSTrackerBucketName, cursor_file_name, os_client)
             logger.debug("Check existence of cursor file in Bucket done")
             if check_existence_cursor_file:  
                 logger.debug("Cursor file is in the bucket")
-                # Step 5: Create/Update Lock File
+                # Step 5: Create/Update Lock File.
                 logger.debug("Create/Update Lock File")
                 content_lock_file=json.dumps({"lock": current_time})
                 put_object_to_bucket(ociOSTrackerBucketName, lock_file_name, os_client, content_lock_file)
-                # Step 6: Get file cursor from ObjectStorage/Bucket 
+                # Step 6: Get file cursor from ObjectStorage/Bucket. 
                 logger.debug("Get File cursor content from Bucket")
                 lastexecutionupdatime = get_object_from_bucket(ociOSTrackerBucketName, cursor_file_name, os_client, lastAuditEventRecordTime_attr)
                 logger.debug("Content from file cursor lasteventexecutiontime: " + lastexecutionupdatime)
                 logger.debug("Content from file cursor done") 
-                # Step 7: Initializing DataSafe Client
+                # Step 7: Initializing DataSafe Client.
                 logger.debug("initializing DataSafe client")
                 data_safe_client = init_datasafe_client(rps)
                 logger.debug("DataSafe client obtained with Instance Principal")
-                # Step 8: Generate SCIM query based on time window
+                # Step 8: Generate SCIM query based on time window.
                 logger.debug("Generate SCIM Query")
                 scim_query= generate_scim_query(lastexecutionupdatime, current_time)
                 logger.debug("Generate SCIM Query Done")
-                # Step 9: Get DB Audit Events from DataSafe
+                # Step 9: Get DB Audit Events from DataSafe.
                 logger.debug("get DB Audit Events from DataSafe")
                 ds_dbaudit_events = get_audit_events(data_safe_client,ociDataSafeCompartmentOCID, sort_order, limit, sort_by, compartment_id_in_subtree, access_level, scim_query, headerTimeCollected, max_auditevents)
                 if not ds_dbaudit_events.empty:
-                    # Step 10: Get Last Event time DB Audit Collected
+                    # Step 10: Get Last Event time DB Audit Collected.
                     lastdbauditeventcolletcted = ds_dbaudit_events[headerTimeCollected].iloc[0]
                     logger.debug("Last DB Audit Time Events Collected from DataSafe: " + str(lastdbauditeventcolletcted))
-                    # Step 11: Initializing OCI Logging ingestion Client
+                    # Step 11: Initializing OCI Logging ingestion Client.
                     logger.debug("initializing OCI Logging ingestion client")
                     oci_logging_ingestion_client = init_logging_ingestion_client(rps)
                     logger.debug("OCI Logging ingestion client obtained with Instance Principal") 
-                    #Step 12: Send DB Audit Events to OCI Logging
+                    #Step 12: Send DB Audit Events to OCI Logging.
                     logger.debug("Send Audit Events to OCI Logging")
                     put_logs(ds_dbaudit_events, oci_logging_ingestion_client, ociLoggingLogOCID, headerAuditEventTime)
                     logger.debug("End Send Audit Events to OCI Logging")
-                    #Step 13: Put Last Time Collect DB Audit Events in cursor file
+                    #Step 13: Put Last Time Collect DB Audit Events in cursor file.
                     logger.debug("Put Last Time Collect DB Audit Events in cursor file in OCI ObjectStorage/Bucket")
                     new_lastdbauditeventcolletcted = last_collected_time(lastdbauditeventcolletcted)
                     content_cursor_file_time=json.dumps({lastAuditEventRecordTime_attr: new_lastdbauditeventcolletcted})
                     put_object_to_bucket(ociOSTrackerBucketName, cursor_file_name, os_client, content_cursor_file_time)
                     logger.debug("End Last Time Collect DB Audit Events in cursor file in OCI ObjectStorage/Bucket")
-                # Step 14: Delete lock file in Bucket
+                # Step 14: Delete lock file in Bucket.
                 logger.debug("Delete lock filein OCI ObjectStorage/Bucket")
                 delete_object_from_bucket(ociOSTrackerBucketName, lock_file_name, os_client)
             else:
