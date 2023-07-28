d581f43bc42cd428881674bc40991131ded65245
Ulrike Schwinn <ulrike.schwinn@oracle.com>
2023-07-28 15:17:23 +0200

    Create active-report.sql

---
+++ data-platform/core-converged-db/sql-performance/sql-monitoring/scripts-sql-monitoring/active-report.sql
@@ -0,0 +1,16 @@
+REM  Script: active-report.sql
+REM  Generate active (HTML) report with DBMS_SQL_MONITOR.REPORT_SQL_MONITOR
+
+-- report on the last statement monitored by Oracle
+-- use type ACTIVE
+
+set trimspool on
+set trim on
+set pagesize 0
+set linesize 32767
+set long 1000000
+set longchunksize 1000000
+spool sqlmon_active.html
+select dbms_sql_monitor.report_sql_monitor(report_level=>'ALL',type=>'ACTIVE')
+from dual;
+spool off
