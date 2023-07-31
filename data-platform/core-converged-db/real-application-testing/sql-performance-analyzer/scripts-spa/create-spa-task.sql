92c1cd96bf2d6c056094fcb75319fa962ddcc2f5
Ulrike Schwinn <ulrike.schwinn@oracle.com>
2023-07-31 17:42:44 +0200

    Update create-spa-task.sql

--- data-platform/core-converged-db/real-application-testing/sql-performance-analyzer/scripts-spa/create-spa-task.sql
+++ data-platform/core-converged-db/real-application-testing/sql-performance-analyzer/scripts-spa/create-spa-task.sql
@@ -1,4 +1,4 @@
-REM Script create-spa-task.sql
+REM Script: create-spa-task.sql
 REM Create a SQL Performance Analyzer task
 
 -- enter STS name and owner of the STS
@@ -8,6 +8,6 @@ set serveroutput on
 declare tname varchar2(100);
 begin    
    tname := dbms_sqlpa.create_analysis_task(sqlset_name => '&STSname', sqlset_owner=>'&STSOwner');
-   dbms_output.put_line( 'task name: ' || tname);
+   dbms_output.put_line('task name: ' || tname);
 end;
 /
