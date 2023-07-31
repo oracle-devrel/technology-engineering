f7b0c188c983a5a8e89339ae6c6109673d0bca25
Ulrike Schwinn <ulrike.schwinn@oracle.com>
2023-07-31 17:43:48 +0200

    Update drop-spa-task.sql

--- data-platform/core-converged-db/real-application-testing/sql-performance-analyzer/scripts-spa/drop-spa-task.sql
+++ data-platform/core-converged-db/real-application-testing/sql-performance-analyzer/scripts-spa/drop-spa-task.sql
@@ -1,6 +1,6 @@
-REM Script drop-spa-task.sql
+REM Script: drop-spa-task.sql
 REM drop SQL Performance Analyzer task 
 
--- enter task name  
+-- enter SPA task name  
 execute dbms_sqlpa.drop_analysis_task(task_name => '&taskname');
 -- check SPA tasks with monitor_task.sql
