7cc21b8028632ec62abf0bf61330f24707481a99
Ulrike Schwinn <ulrike.schwinn@oracle.com>
2023-07-31 16:48:07 +0200

    Create  monitor-executions.sql

---
+++ data-platform/core-converged-db/real-application-testing/sql-performance-analyzer/scripts-spa/monitor-executions.sql
@@ -0,0 +1,10 @@
+REM Script monitor-executions.sql
+REM Monitor SQL Performance Analyzer executions
+
+-- list task name and executions for advisor
+-- use SQL PERFORMANCE ANALYZER in the case of SPA  
+col execution_name format a25
+col task_name format a25 
+
+select task_name, execution_name
+from dba_advisor_executions where upper(advisor_name)='&Advisorname';
