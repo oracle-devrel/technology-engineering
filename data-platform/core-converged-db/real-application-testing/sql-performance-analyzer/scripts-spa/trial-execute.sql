1f1a3baba9196868e06842628d237924288770fa
Ulrike Schwinn <ulrike.schwinn@oracle.com>
2023-07-31 16:51:18 +0200

    Create trial-execute.sql

---
+++ data-platform/core-converged-db/real-application-testing/sql-performance-analyzer/scripts-spa/trial-execute.sql
@@ -0,0 +1,10 @@
+REM Script trial-execute.sql
+REM Execute the trial with execution_type TEST EXECUTE
+
+-- This is the default setting
+-- It executes every SQL statement and collect its execution plans and execution statistics. 
+
+-- enter the task name and add a user defined execution name
+execute DBMS_SQLPA.EXECUTE_ANALYSIS_TASK(task_name       => '&Taskname', -
+                                         execution_type  => 'test execute', -
+                                         execution_name  => '&executionename');
