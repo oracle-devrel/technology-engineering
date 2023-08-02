b0a49ff00dbd04faf3f7afe3c25558daf876c9cf
Ulrike Schwinn <ulrike.schwinn@oracle.com>
2023-08-02 11:25:28 +0200

    Update 4-add-external-partition-data.sql

--- data-platform/core-converged-db/hybrid-partitioned/scripts/4-add-external-partition-data.sql
+++ data-platform/core-converged-db/hybrid-partitioned/scripts/4-add-external-partition-data.sql
@@ -1,8 +1,7 @@
 REM Script: 4-add-external-partition-data.sql  
 REM Use EXCHANGE PARTITION 
 
--- swap data segments to get external data partition
- 
+-- swap data segments to add (external) data to the partition 
 ALTER TABLE hr.employees_hybrid
 EXCHANGE PARTITION(salary_4000) WITH TABLE ext_help;
 
