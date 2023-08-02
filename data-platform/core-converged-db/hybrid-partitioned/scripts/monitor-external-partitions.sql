238a21ff7a48147317749bdb934a93055b6671ef
Ulrike Schwinn <ulrike.schwinn@oracle.com>
2023-08-02 11:23:15 +0200

    Update and rename monitor-external-part-tables.sql to monitor-external-partitions.sql

--- data-platform/core-converged-db/hybrid-partitioned/scripts/monitor-external-partitions.sql
+++ data-platform/core-converged-db/hybrid-partitioned/scripts/monitor-external-partitions.sql
@@ -1,5 +1,5 @@
-REM Script: monitor-external-part-tables.sql
-REM Use ALL_EXTERNAL_TAB_PARTITIONS
+REM Script: monitor-external-partitions.sql
+REM Use ALL_XTERNAL_TAB_PARTITIONS
 
 
 col table_owner format a15
