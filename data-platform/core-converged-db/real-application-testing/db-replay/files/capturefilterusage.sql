3b8c2222ada4f2f234138c71b1ec0ece1941b760
Ulrike Schwinn <ulrike.schwinn@oracle.com>
2024-07-15 13:27:57 +0200

    Update capturefilterusage.sql
    Changed capturefilterusage.sql

--- data-platform/core-converged-db/real-application-testing/db-replay/files/capturefilterusage.sql
+++ data-platform/core-converged-db/real-application-testing/db-replay/files/capturefilterusage.sql
@@ -3,7 +3,7 @@
 -- example 1 for inclusion
  
 -- Add filters
--- The usage mode is determined by the default_action input to the START_CAPTURE Procedure.
+-- The usage mode is determined by the DEFAULT_ACTION input of START_CAPTURE procedure.
 
 exec DBMS_WORKLOAD_CAPTURE.ADD_FILTER (fname => 'USER_TRACK', fattribute => 'USER', fvalue => 'TRACKUSER');
 
@@ -12,9 +12,9 @@ exec DBMS_WORKLOAD_CAPTURE.ADD_FILTER (fname => 'USER_TRACK', fattribute => 'USE
 
 -- start capture (see capturestart.sql) and use EXCLUDE in DEFAULT_ACTION
   
-execute dbms_workload_capture.start_capture (name=>'&Replayname', dir=>'&Reaplydir', default_action=>'EXCLUDE');
+execute dbms_workload_capture.start_capture (name=>'&Name', dir=>'&Dir', default_action=>'EXCLUDE');
 
--- No user request to the database is captured, except for the part of the workload defined by the filter.
+-- EXCLUDE: No user request to the database is captured, except for the part of the workload defined by the filter.
 
 -- check status in dba_workload_filters
 -- check filters_used in dba_workload_captures
@@ -23,17 +23,17 @@ execute dbms_workload_capture.start_capture (name=>'&Replayname', dir=>'&Reaplyd
 -- example 2 for exclusion
 
 -- Add filters
--- The usage mode is determined by the default_action input to the START_CAPTURE Procedure.
+-- The usage mode is determined by the DEFAULT_ACTION input of START_CAPTURE procedure.
 
-exec dbms_workload_capture.add_filter('ORACLE MANAGEMENT SERVICE (DEFAULT)', 'Program', 'OMS');
-exec dbms_workload_capture.add_filter('ORACLE MANAGEMENT AGENT (DEFAULT)', 'Program', 'emagent%');
-exec dbms_workload_capture.add_filter('U_DBSNMP', 'User', 'DBSNMP');
+exec dbms_workload_capture.add_filter('ORACLE_MANAGEMENT_SERVICE', 'Program', 'OMS');
+exec dbms_workload_capture.add_filter('ORACLE_MANAGEMENT_AGENT', 'Program', 'emagent%');
+exec dbms_workload_capture.add_filter('U_DBSNMP', 'USER', 'DBSNMP');
 
 
 -- check status in dba_workload_filters
--- check filters_used dba_workload_captures
+-- check filters_used in dba_workload_captures
 
 
-execute dbms_workload_capture.start_capture (name=>'&Replayname', dir=>'&Reaplydir', default_action=>'INCLUDE');
+execute dbms_workload_capture.start_capture (name=>'&Name', dir=>'&Dir', default_action=>'INCLUDE');
 
--- All user requests to the database are captured, except for the part of the workload defined by the filters. 
+-- INCLUDE: All user requests to the database are captured, except for the part of the workload defined by the filters. 
