fc40c4af38d9c387bbeac8eac5e064ca759f97b9
alcampag <alberto.campagna@oracle.com>
2025-10-27 16:27:04 +0100

    oke-rm-1.1.8

--- app-dev/devops-and-containers/oke/oke-rm/infra/main.tf
+++ app-dev/devops-and-containers/oke/oke-rm/infra/main.tf
@@ -8,41 +8,44 @@ module "network" {
   create_vcn = var.create_vcn
   vcn_id = var.vcn_id
   vcn_name = var.vcn_name
-  vcn_cidr_blocks = var.vcn_cidr_blocks
+  vcn_cidr_blocks = local.vcn_cidr_blocks
   vcn_dns_label = var.vcn_dns_label
   # CP SUBNET
   create_cp_subnet = var.create_cp_subnet
-  cp_subnet_cidr = var.cp_subnet_cidr
-  cp_subnet_dns_label = var.cp_subnet_dns_label
+  cp_subnet_cidr = local.subnets.cidr.cp
+  cp_subnet_dns_label = local.subnets.dns.cp
   cp_subnet_name = var.cp_subnet_name
   cp_subnet_private = var.cp_subnet_private
   cp_allowed_source_cidr = var.cp_allowed_source_cidr
-  # SERVICE SUBNET
-  create_service_subnet = var.create_service_subnet
-  service_subnet_cidr = var.service_subnet_cidr
-  service_subnet_dns_label = var.service_subnet_dns_label
-  service_subnet_name = var.service_subnet_name
-  service_subnet_private = var.service_subnet_private
+  # LB SUBNETS
+  create_external_lb_subnet = var.create_external_lb_subnet
+  external_lb_cidr = local.subnets.cidr.lb_external
+  external_lb_subnet_dns_label = local.subnets.dns.lb_external
+  external_lb_subnet_name = var.external_lb_subnet_name
+  create_internal_lb_subnet = var.create_internal_lb_subnet
+  internal_lb_cidr = local.subnets.cidr.lb_internal
+  internal_lb_subnet_dns_label = local.subnets.dns.lb_internal
+  internal_lb_subnet_name = var.internal_lb_subnet_name
   # WORKER SUBNET
   create_worker_subnet = var.create_worker_subnet
-  worker_subnet_cidr = var.worker_subnet_cidr
-  worker_subnet_dns_label = var.worker_subnet_dns_label
+  worker_subnet_cidr = local.subnets.cidr.worker
+  worker_subnet_dns_label = local.subnets.dns.worker
   worker_subnet_name = var.worker_subnet_name
   # POD SUBNET
   create_pod_subnet = var.create_pod_subnet
-  pod_subnet_cidr = var.pod_subnet_cidr
-  pod_subnet_dns_label = var.pod_subnet_dns_label
+  pod_subnet_cidr = local.subnets.cidr.pod
+  pod_subnet_dns_label = local.subnets.dns.pod
   pod_subnet_name = var.pod_subnet_name
   # BASTION SUBNET
   create_bastion_subnet = var.create_bastion_subnet
-  bastion_subnet_cidr = var.bastion_subnet_cidr
-  bastion_subnet_dns_label = var.bastion_subnet_dns_label
+  bastion_subnet_cidr = local.subnets.cidr.bastion
+  bastion_subnet_dns_label = local.subnets.dns.bastion
   bastion_subnet_name = var.bastion_subnet_name
   bastion_subnet_private = var.bastion_subnet_private
   # FSS SUBNET
   create_fss = var.create_fss
-  fss_subnet_cidr = var.fss_subnet_cidr
-  fss_subnet_dns_label = var.fss_subnet_dns_label
+  fss_subnet_cidr = local.subnets.cidr.fss
+  fss_subnet_dns_label = local.subnets.dns.fss
   fss_subnet_name = var.fss_subnet_name
   # GATEWAYS
   create_gateways = var.create_gateways
---
+++ app-dev/devops-and-containers/oke/oke-rm/infra/modules/network/gateways.tf
@@ -0,0 +1,23 @@
+resource "oci_core_service_gateway" "service_gateway" {
+  compartment_id = var.network_compartment_id
+  vcn_id         = local.vcn_id
+  display_name = "SG"
+  services {
+    service_id = lookup(data.oci_core_services.all_oci_services.services[0], "id")
+  }
+  count = var.create_gateways ? 1 : 0
+}
+
+resource "oci_core_nat_gateway" "nat_gateway" {
+  compartment_id = var.network_compartment_id
+  vcn_id         = local.vcn_id
+  display_name = "NAT"
+  count = var.create_gateways ? 1 : 0
+}
+
+resource "oci_core_internet_gateway" "internet_gateway" {
+  compartment_id = var.network_compartment_id
+  vcn_id         = local.vcn_id
+  display_name = "IG"
+  count = local.all_subnet_private && ! local.create_internet_gateway ? 0 : 1
+}
