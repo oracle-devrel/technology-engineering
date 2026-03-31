50e89b5a45131387cc234ed8387991ed68c3706e
cghirardi.oracle <cristiano.ghirardi@oracle.com>
2026-03-31 11:20:02 +0200

    ver. 0.1

---
+++ cloud-architecture/architecture-coe/fsdr/assets/network-insulation-during-drill/files/functions/hello/func.py
@@ -0,0 +1,21 @@
+import io
+import json
+import logging
+
+from fdk import response
+
+
+def handler(ctx, data: io.BytesIO = None):
+    name = "World"
+    try:
+        body = json.loads(data.getvalue())
+        name = body.get("name")
+    except (Exception, ValueError) as ex:
+        logging.getLogger().info('error parsing json payload: ' + str(ex))
+
+    logging.getLogger().info("Inside Python Hello World function")
+    return response.Response(
+        ctx, response_data=json.dumps(
+            {"message": "Hello {0}".format(name)}),
+        headers={"Content-Type": "application/json"}
+    )
