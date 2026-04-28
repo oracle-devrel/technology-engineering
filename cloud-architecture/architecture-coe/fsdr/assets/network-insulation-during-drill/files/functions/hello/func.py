a51a997546c851329cb4692b2dbe982de18d0833
Cristiano Ghirardi <cristiano.ghirardi@oracle.com>
2026-04-28 17:45:40 +0200

    Add copyright and licensing details to func.py
    Added copyright and licensing information to the script.

--- cloud-architecture/architecture-coe/fsdr/assets/network-insulation-during-drill/files/functions/hello/func.py
+++ cloud-architecture/architecture-coe/fsdr/assets/network-insulation-during-drill/files/functions/hello/func.py
@@ -1,3 +1,20 @@
+# -----------------------------------------------------------------------------
+# Copyright (c) 2026 Oracle and/or its affiliates.
+#
+# Licensed under the Universal Permissive License v 1.0 as shown at
+# https://oss.oracle.com/licenses/upl/
+#
+# You may use, copy, modify, and distribute this software and its documentation
+# under the terms of the UPL. This software is provided "AS IS" without warranty
+# of any kind.
+#
+# Oracle does not provide support for this script; community support only.
+# This software is provided as it is and need to be considered an example
+# of a possible implementation rather than for general use. 
+#
+# Script provided by: Cristiano Ghirardi, Oracle employee
+# -----------------------------------------------------------------------------
+
 import io
 import json
 import logging
